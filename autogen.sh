2a6394ca3e2dd8c2e0b6d77503388e870ae63990
newbiz <vallee.aurelien@gmail.com>
2014-02-12 22:10:41 +0200

    Finish source and debian release management. Usage: cd release && make [source|debian]

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-hash autoreconf 2> /dev/null || { echo >&2 "error: autoreconf is required to run this script"; exit 1; }
-
-autoreconf --install 2>&1
+autoreconf --force --install "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
