95e4f2887044c38f01321d4a366208145da578c1
newbiz <vallee.aurelien@gmail.com>
2014-03-13 20:16:00 +0200

    Add gbemu-play binary target

--- gbemu-play/src/gbemu-as.cpp
+++ gbemu-play/src/gbemu-as.cpp
@@ -7,15 +7,18 @@
 
 int main( int argc, char** argv )
 {
-    std::cout << "gbemu-as" << std::endl;
+    std::cout << "gbemu-play" << std::endl;
     std::cout << "Using gbemu-lib " << gbemu::version() << std::endl;
   
     if (argc!=2)
     {
-        std::cerr << "usage: ./gbemu-as <assembly_file>" << std::endl;
+        std::cerr << "usage: ./gbemu-play <cartridge_file>" << std::endl;
         return EXIT_FAILURE;
     }
 
+    gbemu::Cartridge cart(argv[1]);
+    cart.dump(std::cout);
+    
     return EXIT_SUCCESS;
 }
 
